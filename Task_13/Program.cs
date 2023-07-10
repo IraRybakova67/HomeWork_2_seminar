bd3313a334670b87dea166878cfe1e7f8926c5d4
IraRybakova67 <rda-67@mail.ru>
2023-07-10 21:02:48 +0300

    Решение 13 задачи

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите  число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+int ten =(int)Math.Log10(num);
+// Находит разрядность числа
+int a = num / (int)Math.Pow(10, ten-2);
+// Делит число на необходимое  в 10 раз для получения трехзначного
+int ThirdFigure = a % 10;
+Console.WriteLine($"Третья цифра в числе {num} -> {ThirdFigure}");
+}
+else
+{
+   Console.WriteLine("В этом числе нет третьей цифры");
+}
